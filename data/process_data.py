bd7a2fb7ebdaef6fac120bd764d3121eeacac6d4
alshafai <alshafai@berkeley.edu>
2019-07-17 19:53:52 +0000

    written a working process_data file and in progress train_classifier

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,61 @@
 import sys
-
+import pandas as pd
+import numpy as np
+import sqlalchemy
+import sqlite3
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """ A function that loads the messages data and the categories data and merge them
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id', how='outer')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    """ A function that cleans the data
+    """
+    # split from all values in one string to each value in a different column
+    categories = df.categories.str.split(';', expand= True)
+    
+    # Change column names
+    row = categories.loc[0,:]
+    category_colnames = list(row.apply(lambda x: x.split('-')[0]))
+    categories.columns = category_colnames
+    
+    for column in categories:
+    # set each value to be the last character of the string to get the numeric
+        categories[column] = categories[column].apply(lambda x: x.split('-')[1])
+    
+    # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    
+    # drop the original categories column from `df`
+    df.drop(['categories'], axis=1, inplace= True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis= 1)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace= True)
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """ A function that saves the data to sql database
+    """
+#     conn = sqlite3.connect(database_filename)
+#     df.to_sql('messages',conn)
+#     conn.close
+
+
+    engine = sqlalchemy.create_engine('sqlite:///'+database_filename)
+#     print('sqlite:///'+database_filename)
+    df.to_sql('messages', engine, index=False, if_exists = 'append')
+    
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,20 +1,79 @@
 import sys
-
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import re
+from sklearn.metrics import classification_report
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger','stopwords'])
 
 def load_data(database_filepath):
-    pass
-
+    """ A functin that loads the data from the database
+    """
+    # load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('messages2', engine)
+    X = df.message
+    Y = df.drop(['id','message','original','genre'], axis= 1)
 
 def tokenize(text):
-    pass
+    """ A function that tokenize the text 
+    """
+    # normalize case and remove punctuation
+    stop_words = set(stopwords.words("english"))
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # lemmatize andremove stop words
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word).lower().strip() for word in tokens if word not in stop_words]
+
+    return tokens
 
 
 def build_model():
-    pass
+    """ A function that builds the model
+    """
+    pipeline = Pipeline([
+        ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
 
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = {
+        'text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
+        'text_pipeline__vect__max_features': (None, 5000, 10000),
+        'text_pipeline__tfidf__use_idf': (True, False),
+        'clf__n_estimators': [50, 100, 200],
+        'clf__min_samples_split': [2, 3, 4],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    cv.fit(X_train, Y_train)
+    
+    clf = cv.best_estimator_
+    
+    return clf
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    for col in y_pred.columns:
+        print(classification_report(Y_test, y_pred, target_names= category_names))
 
 
 def save_model(model, model_filepath):
